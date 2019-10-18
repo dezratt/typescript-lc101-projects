3960dedd6d3ff1ba9fdad509deb1d0b3acdc81d9
David <dezratty12@gmail.com>
2019-10-17 19:33:52 -0500

    commit for completed project

---
+++ studio/Rocket.ts
@@ -0,0 +1,61 @@
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+import { Payload } from './Payload';
+
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+        constructor(name: string, totalCapacityKg: number) {
+            this.name = name;
+            this.totalCapacityKg = totalCapacityKg
+    }
+    sumMass( items: Payload[] ): number{
+        let sum;
+        for (let i = 0; i < items.length; i++) {
+            sum += items[i].massKg;
+        } return sum; 
+    }
+
+    currentMassKg(): number {
+        let mass = this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        return mass;
+    } 
+    canAdd(item: Payload): boolean {
+        if ((this.currentMassKg() + item.massKg) <= this.totalCapacityKg) {
+            return true;
+        }
+    }
+    addCargo(cargo: Cargo) {
+        if (this.canAdd) {
+            this.cargoItems.push(cargo);       
+        } else {
+            return false;
+        }
+    }
+    addAstronaut(astronaut: Astronaut) {
+        if (this.canAdd) {
+            this.astronauts.push(astronaut)
+            return true
+        } else {
+            return false;
+        }
+    }
+}
+
+
+
+
+
+
+// massKg: number;
+// name: string;
+
+//     constructor(massKg: number, name: string) {
+//         this.massKg = massKg
+//         this.name = name
+//     }
+// }
